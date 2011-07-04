25af11dee5b95a31a3e260e60311378509088b2f
Francois Reynaud <francois.reynaud@gmail.com>
2011-07-04 15:37:06 +0100

    base mapping for global

--- src/test/java/CommandTests.java
+++ src/test/java/CommandTests.java
@@ -8,14 +8,15 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class CommandTests {
 
-	protected final int load = 10; 
-	
+	protected final int load = 1;
+
 	Driver d = new Driver();
 
 	@BeforeClass
@@ -23,7 +24,7 @@ public class CommandTests {
 		d.start();
 	}
 
-	@Test(invocationCount=load,threadPoolSize=load)
+	@Test(invocationCount = load, threadPoolSize = load)
 	public void getTitle() throws InterruptedException, IOException {
 		WebDriver driver = null;
 		try {
@@ -36,8 +37,8 @@ public class CommandTests {
 		}
 
 	}
-	
-	@Test(invocationCount=load,threadPoolSize=load,expectedExceptions = NoSuchElementException.class)
+
+	@Test(invocationCount = load, threadPoolSize = load, expectedExceptions = NoSuchElementException.class)
 	public void getElementNeg() throws InterruptedException, IOException {
 		WebDriver driver = null;
 		try {
@@ -49,35 +50,34 @@ public class CommandTests {
 
 		}
 	}
-	
 
-	@Test(invocationCount=load,threadPoolSize=load)
+	@Test(invocationCount = load, threadPoolSize = load)
 	public void getElement() throws InterruptedException, IOException {
 		WebDriver driver = null;
 		try {
 			driver = new RemoteWebDriver(new URL("http://localhost:9999/wd/hub"), DesiredCapabilities.firefox());
-			driver.get("http://ebay.co.uk");	
+			driver.get("http://ebay.co.uk");
 			WebElement el = driver.findElement(By.id("registerLink"));
 		} finally {
 			driver.quit();
 
 		}
 	}
-	
-	@Test(invocationCount=load,threadPoolSize=load)
+
+	@Test(invocationCount = load, threadPoolSize = load)
 	public void setValue() throws InterruptedException, IOException {
 		WebDriver driver = null;
 		try {
 			driver = new RemoteWebDriver(new URL("http://localhost:9999/wd/hub"), DesiredCapabilities.firefox());
-			driver.get("http://ebay.co.uk");	
+			driver.get("http://ebay.co.uk");
 			WebElement el = driver.findElement(By.id("_nkw"));
 			el.sendKeys("ferret");
 		} finally {
 			driver.quit();
 		}
 	}
-	
-	@Test(invocationCount=load,threadPoolSize=load)
+
+	@Test(invocationCount = load, threadPoolSize = load)
 	public void setValueGoogle() throws InterruptedException, IOException {
 		WebDriver driver = null;
 		try {
@@ -89,22 +89,32 @@ public class CommandTests {
 			driver.quit();
 		}
 	}
-	
-	@Test(invocationCount=load,threadPoolSize=load)
+
+	@Test(invocationCount = load, threadPoolSize = load)
 	public void click() throws InterruptedException, IOException {
 		WebDriver driver = null;
 		try {
 			driver = new RemoteWebDriver(new URL("http://localhost:9999/wd/hub"), DesiredCapabilities.firefox());
-			driver.get("http://ebay.co.uk");	
+			driver.get("http://ebay.co.uk");
 			WebElement el = driver.findElement(By.id("registerLink"));
 			el.click();
 		} finally {
 			driver.quit();
 		}
 	}
-	
-	
-	
+
+	@Test(invocationCount = load, threadPoolSize = load)
+	public void getURL() throws InterruptedException, IOException {
+		WebDriver driver = null;
+		try {
+			String url = "http://www.ebay.co.uk/";
+			driver = new RemoteWebDriver(new URL("http://localhost:9999/wd/hub"), DesiredCapabilities.firefox());
+			driver.get(url);
+			Assert.assertEquals(driver.getCurrentUrl(), url);
+		} finally {
+			driver.quit();
+		}
+	}
 
 	@AfterClass
 	public void stop() throws InterruptedException {
